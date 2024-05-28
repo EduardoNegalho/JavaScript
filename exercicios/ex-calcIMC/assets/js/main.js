341030a1f50724fdd975c907938a53c7218a4c44
Eduardo Marques <202208702007@alunos.estacio.br>
2024-05-28 08:30:05 -0300

    melhorando o codigo

--- exercicios/ex-calcIMC/assets/js/main.js
+++ exercicios/ex-calcIMC/assets/js/main.js
@@ -4,40 +4,53 @@ function main() {
     const calculateBMI = (weight, height) => {
         const bmi = weight / Math.pow(height, 2);
 
-        if (bmi < 18.5) {
-            return `Seu IMC é ${bmi.toFixed(2)} (Abaixo do peso)`;
-        } else if (bmi <= 24.9) {
-            return `Seu IMC é ${bmi.toFixed(2)} (Peso normal)`;
-        } else if (bmi <= 29.9) {
-            return `Seu IMC é ${bmi.toFixed(2)} (Sobrepeso)`;
-        } else if (bmi <= 34.9) {
-            return `Seu IMC é ${bmi.toFixed(2)} (Obesidade grau 1)`;
-        } else if (bmi <= 39.9) {
-            return `Seu IMC é ${bmi.toFixed(2)} (Obesidade grau 2)`;
-        } else {
-            return `Seu IMC é ${bmi.toFixed(2)} (Obesidade grau 3)`;
-        }
+        if (bmi < 18.5) return `Seu IMC é ${bmi.toFixed(2)} (Abaixo do peso)`;
+
+        if (bmi <= 24.9)  return `Seu IMC é ${bmi.toFixed(2)} (Peso normal)`;
+
+        if (bmi <= 29.9) return `Seu IMC é ${bmi.toFixed(2)} (Sobrepeso)`;
+
+        if (bmi <= 34.9) return `Seu IMC é ${bmi.toFixed(2)} (Obesidade grau 1)`;
+
+        if (bmi <= 39.9) return `Seu IMC é ${bmi.toFixed(2)} (Obesidade grau 2)`;
+
+        if (bmi >= 40) return `Seu IMC é ${bmi.toFixed(2)} (Obesidade grau 3)`;
     }
 
+    const showResult = (msg, isValid) => {
+        const divResult = document.querySelector('#result');
+        divResult.innerHTML = "";
+
+        const className = isValid === true ? 'paragraph-result-true' : 'paragraph-result-false';
+
+        const p = document.createElement('p');
+        p.classList.add(className);
+        p.innerHTML = msg;
+        divResult.appendChild(p);
+    }
 
     form.addEventListener('submit', (evt) => {
         evt.preventDefault();
 
-        const weight = form.querySelector('#weight').value;
-        const height = form.querySelector('#height').value;
+        const inputWeight = evt.target.querySelector('#weight');
+        const inputHeight = evt.target.querySelector('#height');
 
-        const result = calculateBMI(weight, height);
+        const weight = Number(inputWeight.value);
+        const height = Number(inputHeight.value);
 
-        const showResult = () => {
-            const divResult = document.querySelector('#result');
-
-            divResult.textContent = result;
+        if (!weight) {
+            showResult("Peso Inválido", false);
+            return
         }
 
-        showResult();
+        if (!height) {
+            showResult("Altura Inválida", false);
+            return
+        }
+
+        const bmi = calculateBMI(weight, height);
+        showResult(bmi, true);
     })
-
-
 }
 
 main()
